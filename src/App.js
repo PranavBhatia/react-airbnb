9d63f85a4076c75df528581c1dafa86abacbda81
Pranav <bhatiapranav1996@gmail.com>
2021-06-22 23:59:47 -0400

    Adding Links & Navigation

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { Route, Switch } from "react-router-dom";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupsPage from "./pages/NewMeetup";
 import AllMeetupsPage from "./pages/AllMeetups";
+import MainNavigation from "./components/layout/MainNavigation";
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact>
           <AllMeetupsPage />
